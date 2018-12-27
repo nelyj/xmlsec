c7ae05b1874c9063d9d257732ab0700799824212
Nelson Patricio Jimenez <developer.jimenez@gmail.com>
2018-12-27 15:40:56 -0300

    fix xmlSecTransformInclC14NId

--- ext/nokogiri_ext_xmlsec/nokogiri_sign_rsa.c
+++ ext/nokogiri_ext_xmlsec/nokogiri_sign_rsa.c
@@ -21,7 +21,7 @@ VALUE sign_with_key(VALUE self, VALUE rb_key_name, VALUE rb_rsa_key) {
   // create signature template for RSA-SHA1 enveloped signature
   //signNode = xmlSecTmplSignatureCreate(doc, xmlSecTransformExclC14NId,
   //                                       xmlSecTransformRsaSha1Id, NULL);
-  signNode = xmlSecTmplSignatureCreate(doc, xmlSecTransformIncC14NId,
+  signNode = xmlSecTmplSignatureCreate(doc, xmlSecTransformInclC14NId,
                                          xmlSecTransformRsaSha1Id, NULL);
   if (signNode == NULL) {
     rb_raise(rb_eSigningError, "failed to create signature template");
