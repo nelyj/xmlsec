5420b52c9c51cd0354d42ab885c63e9ee42d23b3
Nelson Patricio Jimenez <developer.jimenez@gmail.com>
2018-12-27 21:13:19 -0300

    test without if

--- ext/nokogiri_ext_xmlsec/nokogiri_sign_certificate.c
+++ ext/nokogiri_ext_xmlsec/nokogiri_sign_certificate.c
@@ -33,11 +33,11 @@ VALUE sign_with_certificate(VALUE self, VALUE rb_key_name, VALUE rb_rsa_key, VAL
   xmlAddChild(xmlDocGetRootElement(doc), signNode);
 
   // add reference
-  if(uri == "#") {
-    refNode = xmlSecTmplSignatureAddReference(signNode, xmlSecTransformSha1Id, NULL, NULL, NULL);
-  else {
+//  if(uri == "#") {
+//    refNode = xmlSecTmplSignatureAddReference(signNode, xmlSecTransformSha1Id, NULL, NULL, NULL);
+ // else {
     refNode = xmlSecTmplSignatureAddReference(signNode, xmlSecTransformSha1Id, NULL, uri, NULL);
-  }
+ // }
 
   if(refNode == NULL) {
     rb_raise(rb_eSigningError, "failed to add reference to signature template");
