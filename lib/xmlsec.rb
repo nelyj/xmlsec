b56bfbb270424059810f9e677526bf0c8469e6da
Nelson Patricio Jimenez <developer.jimenez@gmail.com>
2018-12-30 22:09:23 -0300

    more changes for test

--- lib/xmlsec.rb
+++ lib/xmlsec.rb
@@ -23,7 +23,7 @@ class Nokogiri::XML::Document
         sign_with_certificate opts[:name].to_s, opts[:key], cert, opts[:uri]
       else
         puts "uri nil"
-        sign_with_certificate opts[:name].to_s, opts[:key], cert, "#"
+        sign_with_certificate opts[:name].to_s, opts[:key], cert, ""
       end
     elsif opts[:key]
       sign_with_key opts[:name].to_s, opts[:key]
