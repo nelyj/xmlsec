a4f39b52932dbe0361f83f7b5bae9500b217ea9d
Nelson Patricio Jimenez <developer.jimenez@gmail.com>
2019-01-14 02:30:18 -0300

    new version

--- ext/nokogiri_ext_xmlsec/nokogiri_helpers_set_attribute_id.c
+++ ext/nokogiri_ext_xmlsec/nokogiri_helpers_set_attribute_id.c
@@ -27,37 +27,64 @@ VALUE set_id_attribute(VALUE self, VALUE rb_attr_name) {
 
   xmlNodeSetPtr nodes = xpathObj->nodesetval;
   int size = (nodes) ? nodes->nodeNr : 0;
-
-  printf("Nodes found: %i", size);
+  char* idNames[] = {"ID", "Id", "id"};
+  printf("Nodes found: %i \n", size);
   printf("rb_attr_name: %s \n", idName);
 
-  // find pointer to id attribute
-  attr = xmlHasProp(node, idName);
-  if((attr == NULL) || (attr->children == NULL)) {
-    rb_raise(rb_eRuntimeError, "Can't find attribute to add register as id");
-    return Qfalse;
+  xmlAttrPtr attr, tmp;
+  int i,j;
+  for(i = 0; i < size; i++) {
+    for(j=0; j<3;j++) {
+      tmp = xmlHasProp(nodes->nodeTab[i], idNames[j]);
+      if(tmp != NULL) {
+        attr = tmp;
+      }
+    }
+    if(attr == NULL) {
+      xmlXPathFreeContext(xpathCtx); 
+      return(-1);
+    }
+    xmlChar* name = xmlNodeListGetString(doc, attr->children, 1);
+    printf("name: %s \n", name);
+    if(name == NULL) {
+      xmlXPathFreeContext(xpathCtx); 
+      return(-1);
+    }
+    xmlAttrPtr tmp = xmlGetID(doc, name);
+    if(tmp != NULL) {
+      xmlFree(name);
+      return 0;
+    }
+    xmlAddID(NULL, doc, name, attr);
+    xmlFree(name);
   }
+  // find pointer to id attribute
+  //attr = xmlHasProp(node, idName);
+  //if((attr == NULL) || (attr->children == NULL)) {
+  //  rb_raise(rb_eRuntimeError, "Can't find attribute to add register as id");
+  //  return Qfalse;
+  //}
   
   // get the attribute (id) value
-  name = xmlNodeListGetString(node->doc, attr->children, 1);
-  if(name == NULL) {
-    rb_raise(rb_eRuntimeError, "Attribute %s has no value", idName);
-    return Qfalse;
-  }
+  //name = xmlNodeListGetString(node->doc, attr->children, 1);
+  //if(name == NULL) {
+  //  rb_raise(rb_eRuntimeError, "Attribute %s has no value", idName);
+  //  return Qfalse;
+  //}
   
   // check that we don't have that id already registered
-  tmp = xmlGetID(node->doc, name);
-  if(tmp != NULL) {
+  //tmp = xmlGetID(node->doc, name);
+  //if(tmp != NULL) {
     // rb_raise(rb_eRuntimeError, "Attribute %s is already an ID", idName);
-    xmlFree(name);
-    return Qfalse;
-  }
+  //  xmlFree(name);
+  //  return Qfalse;
+  //}
   
   // finally register id
-  xmlAddID(NULL, node->doc, name, attr);
+  //xmlAddID(NULL, node->doc, name, attr);
 
   // and do not forget to cleanup
-  xmlFree(name);
+  //xmlFree(name);
 
   return Qtrue;
 }
